34941f3345d9a6a42d4e29908bff543b6eb1b2cf
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-05-05 14:38:04 +0545

    Completed delete action

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -7,8 +7,30 @@ use Livewire\Component;
 
 class PostList extends Component
 {
+    public $posts;
+
+    public function mount()
+    {
+        
+        $this->posts = Post::latest()->get();
+
+    }
+    public function delete($id)
+    {
+        $post = Post::find($id);
+
+        if($post)
+        {
+            $post->delete();
+            session()->flash('message', 'Post deleted successfully');
+            $this->posts = Post::latest()->get(); // Refresh the list
+        }
+    }
+
+
     public function render()
     {
-        return view('livewire.post-list',['posts'=>Post::latest()->get()]);
+        // return view('livewire.post-list',['posts'=>Post::latest()->get()]);
+        return view('livewire.post-list');
     }
 }
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,11 +1,17 @@
 <div>
     {{-- Success is as dangerous as failure. --}}
 
-        @if (session()->has('message'))
-        <div class="bg-green-100 text-green-800 p-2 rounded mb-4">
-            {{ session('message') }}
-        </div>
-    @endif
+    @if (session()->has('message'))
+    <div 
+        x-data="{ show: true }" 
+        x-init="setTimeout(() => show = false, 5000)" 
+        x-show="show"
+        class="bg-green-100 text-green-800 p-2 rounded mb-4"
+    >
+        {{ session('message') }}
+    </div>
+@endif
+
 
 
     <div class="max-w-2xl mx-auto mt-10 space-y-4">
@@ -21,6 +27,11 @@
 
                 <a href="{{route('posts.edit',$post->id)}}" class="bg-blue-900 text-white px-2 py-1 m-2 hover:underline">Edit</a>
     
+                <button onclick="if(confirm('Are u want to delete this post?')){@this.call('delete',{{$post->id}})}" class="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700">
+                    {{-- onclick="if(confirm('Are u want to delete this post?'))  = This is the inline javascript onclick event handler showw the confirmation if u want to delete the post--}}
+                    {{-- @this.call => this is the livewire mthid to call a method on current livewire component , delete => name of the method inside your current livewire component, $post->id =>Passes the current post’s ID to the method. --}}
+                    Delete
+                </button>
                 <!-- Created At -->
                 <p class="text-sm text-gray-500 mt-1">
                     {{ $post->updated_at->diffForHumans() }}
