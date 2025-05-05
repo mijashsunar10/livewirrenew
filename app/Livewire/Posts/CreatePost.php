25579a89e47ace2f2084a605ba2e1aa8021130f2
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-05-05 07:29:17 +0545

    user name called outside the render function and inside the render function sepreately

--- app/Livewire/Posts/CreatePost.php
+++ app/Livewire/Posts/CreatePost.php
@@ -2,14 +2,24 @@
 
 namespace App\Livewire\Posts;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CreatePost extends Component
 {
 
     public $title = 'my first title';
+    public $name1 ;
+
+    public function mount()
+    {
+        $this->name1 = Auth::user()->name;
+        
+    }
     public function render()
     {
-        return view('livewire.posts.create-post');
+        return view('livewire.posts.create-post')->with(['author'=>Auth::user()->name,'teacher'=>$this->name1,]);
     }
+    
+  
 }
--- resources/views/livewire/posts/create-post.blade.php
+++ resources/views/livewire/posts/create-post.blade.php
@@ -1,6 +1,10 @@
 <div>
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
 
+
    Title: {{$title}}
+   <p>Name: {{$author}}</p>
+   <p>Name:{{$teacher}}</p>
+
    <livewire:create-post/>
 </div>
